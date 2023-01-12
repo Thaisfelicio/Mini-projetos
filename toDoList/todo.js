87df23f0f229c4093b6b8b3e12df383462a52eb6
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2023-01-12 15:26:01 -0300

    Adição do arquivo javascript

---
+++ toDoList/todo.js
@@ -0,0 +1,7 @@
+//a tarefa completa
+let item = document.getElementsByClassName("item");
+
+//botoes da lista
+let salvarTarefa = document.getElementById("salvarTarefa");
+let deletar = document.getElementById("deletar");
+let adicionarTarefa = document.getElementById("adicionarTarefa");
