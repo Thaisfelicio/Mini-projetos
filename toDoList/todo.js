15a64d47132a41fd90382a91ee8348303cde580c
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2023-01-13 23:44:26 -0300

    adicionando evento de clique no botão plus

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -1,7 +1,14 @@
 //a tarefa completa
 let item = document.getElementsByClassName("item");
+let bordaVidro = document.querySelector(".border");
 
 //botoes da lista
 let salvarTarefa = document.getElementById("salvarTarefa");
 let deletar = document.getElementById("deletar");
 let adicionarTarefa = document.getElementById("adicionarTarefa");
+
+adicionarTarefa.addEventListener("click", ()=>{
+    let itemNovo = document.createElement("div");
+    itemNovo.classList.add("item");
+    bordaVidro.appendChild(itemNovo);
+})
