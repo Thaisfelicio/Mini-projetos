6d9bc824377b851dbe7261f3a611dc85707db42f
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2023-01-15 20:23:59 -0300

    Adição da função de adicionar uma tarefa

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -8,7 +8,29 @@ let deletar = document.getElementById("deletar");
 let adicionarTarefa = document.getElementById("adicionarTarefa");
 
 adicionarTarefa.addEventListener("click", ()=>{
-    let itemNovo = document.createElement("div");
-    itemNovo.classList.add("item");
-    bordaVidro.appendChild(itemNovo);
+    //crio uma div nova para armazenar o conteudo
+    let proximoItem = document.createElement("div");
+    //adiciono a classe item nela
+    proximoItem.classList.add("item");
+
+    //crio uma variavel com o conteudo da div
+    let itemNovo = 
+        "<span>" + "<i class='fa-solid fa-hands-clapping'>" + "</i>" +"</span>" +
+        "<input type='text'>" +
+        "<a id='salvarTarefa'>" + "<i class='fa-solid fa-floppy-disk'>" + "</i>" + "</a>" +
+        "<a id='deletar'>" + "<i class='fa-solid fa-trash'>" + "</i>" + "</a>" +
+        "<a id='adicionarTarefa'>" + "<i class='fa-solid fa-plus'>" + "</i>" + "</a>";
+    
+    //adiciono o conteudo criado dentro da div
+    proximoItem.innerHTML = itemNovo;   
+    //adiciono a div completa dentro da div com borda de vidro através do appendChild
+    bordaVidro.appendChild(proximoItem);
 })
+
+// <div class="item">
+    //         <span><i class="fa-solid fa-hands-clapping"></i></span>
+    //         <input type="text">
+    //         <a href="#" id="salvarTarefa"><i class="fa-solid fa-floppy-disk"></i></a>
+    //         <a href="#" id="deletar"><i class="fa-solid fa-trash"></i></a>
+    //         <a href="#" id="adicionarTarefa"><i class="fa-solid fa-plus"></i></a>
+    //     </div>
