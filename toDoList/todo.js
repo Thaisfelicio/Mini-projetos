bab2c53274363a4fb454ef0aed048edc9265e7bf
Thaís Felício de Paula Silva <83433694+Thaisfelicio@users.noreply.github.com>
2023-02-13 19:26:11 -0300

    adição do checkbox para deletar task

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -4,7 +4,7 @@ let bordaVidro = document.querySelector(".border");
 
 //botoes da lista
 let salvarTarefa = document.getElementsByClassName("salvarTarefa");
-let deletar = document.getElementById("deletar");
+let deletar = document.querySelectorAll(".deletar");
 let adicionarTarefa = document.querySelectorAll(".adicionarTarefa");
 
 function addTask(){
@@ -15,24 +15,37 @@ function addTask(){
 
     //crio uma variavel com o conteudo da div
     let itemNovo = 
-        "<span>" + "<i class='fa-solid fa-hands-clapping'>" + "</i>" +"</span>" +
+        "<input type='checkbox' class='selecionar'>" + "</input>" + 
         "<input type='text'>" +
         "<a class='salvarTarefa'>" + "<i class='fa-solid fa-floppy-disk'>" + "</i>" + "</a>" +
-        "<a id='deletar'>" + "<i class='fa-solid fa-trash'>" + "</i>" + "</a>" /*+
+        "<a class='deletar'>" + "<i class='fa-solid fa-trash'>" + "</i>" + "</a>" /*+
         "<a class='adicionarTarefa'>" + "<i class='fa-solid fa-plus'>" + "</i>" + "</a>"*/;
     
     //adiciono o conteudo criado dentro da div
     proximoItem.innerHTML = itemNovo;   
     //adiciono a div completa dentro da div com borda de vidro através do appendChild
-    bordaVidro.appendChild(proximoItem);
+    let newItem = bordaVidro.appendChild(proximoItem);
+    return newItem;
 }
 
     adicionarTarefa[0].addEventListener("click", ()=>{
         addTask();
 })
-
  
+salvarTarefa[0].addEventListener("click", function(tarefa){
+    let input = document.querySelector("input");
+    let tarefaNova = document.createElement("h4");
+    tarefaNova = document.createTextNode(tarefa)
+    input.replaceChild(tarefaNova, item);
+})
 
 
+// item.forEach(item => {
+//     item.addEventListener('click', function(e){
+//         const itemFeito = e.target.parentElement;
+//         itemFeito.remove();
+//     })
+// });
 
 
+  
